3d727626c5bf61a331f757455e323702a8cd110f
Kaz-OS <estebanrattier27@gmail.com>
2026-03-10 10:32:34 +0100

    feat: enhance registration form with role selection and additional fields

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Users;
+use App\Entity\Roles;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,7 +19,14 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('nom')
+            ->add('prenom')
             ->add('email')
+            ->add('idRole', EntityType::class, [
+                'class' => Roles::class,
+                'choice_label' => 'nom',
+                'placeholder' => 'Choisissez un rôle',
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
