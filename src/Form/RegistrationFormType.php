f475341a89bfd960d5b36e3c71cad88b41ee43f7
Yst0s <lacailleyanis@gmail.com>
2026-03-10 11:43:34 +0100

    Mes modifs inscription/connexion

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -33,18 +33,10 @@ class RegistrationFormType extends AbstractType
                     ),
                 ],
             ])
-            ->add('idRole', EntityType::class, [
+            ->add('Role', EntityType::class, [
                 'class' => Roles::class,
                 'choice_label' => 'nom',
                 'placeholder' => 'Choisissez un rôle',
-            ])
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue(
-                        message: 'You should agree to our terms.',
-                    ),
-                ],
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
