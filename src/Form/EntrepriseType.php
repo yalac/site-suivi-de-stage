358993c33953a5c5aac86bffe9aed173132991af
TyrosMLG <c.riviere7619@laposte.net>
2026-04-07 12:47:53 +0200

    feat: implement CRUD functionality for Entreprises with new controller, forms, and templates

---
+++ src/Form/EntrepriseType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Entreprise;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EntrepriseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomEntreprise')
+            ->add('adresseEntreprise')
+            ->add('cpentreprise')
+            ->add('villeEntreprise')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Entreprise::class,
+        ]);
+    }
+}
