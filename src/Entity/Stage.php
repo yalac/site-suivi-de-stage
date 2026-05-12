f3671a727e78bd6dd0f22c731192b2693d447271
yalac <lacailleyanis@gmail.com>
2026-05-12 09:29:50 +0200

    Update Stage.php

--- src/Entity/Stage.php
+++ src/Entity/Stage.php
@@ -94,12 +94,10 @@ class Stage
 
     public function setEleveStage(?Eleve $eleveStage): static
     {
-        // unset the owning side of the relation if necessary
         if ($eleveStage === null && $this->eleveStage !== null) {
             $this->eleveStage->setStageEleve(null);
         }
 
-        // set the owning side of the relation if necessary
         if ($eleveStage !== null && $eleveStage->getStageEleve() !== $this) {
             $eleveStage->setStageEleve($this);
         }
